8a595de9f2d9a31665f9805b49ecce3b1eef06fb
Giovanni Pizani <giovanni.pizani13@gmail.com>
2025-04-29 23:28:18 -0300

    Add files via upload

---
+++ Welcome to C language.c
@@ -0,0 +1,12 @@
+#include<stdio.h> //biblioteca padrão para entrada e saida
+#include<stdlib.h> //biblioteca para usar o comando system("pause")
+/*Programa principal*/
+int main(){
+	printf("Seja bem vindo a linguagem C!\n");
+	system ("pause");
+	return 0;
+	
+}
+
+
+
